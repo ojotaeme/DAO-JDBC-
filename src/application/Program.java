95c46aa413293c1f7e08ba4cdd8d671f61c6eab4
Joao Marcelo <joao.mm.santos@outlook.com>
2024-01-26 18:39:29 -0300

    update seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -34,6 +34,13 @@ public class Program {
 		Seller newSeller = new Seller(null, "Greg", "greg@email.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
+		System.out.println();
+		
+		System.out.println("\n===== TEST 4: seller insert =====");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Wayne");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
 
 	}
 
